a7b600cd688aa301cb95033dea2b7110433499ef
kerry2259 <65342456+kerry2259@users.noreply.github.com>
2020-06-24 17:52:35 +0900

    ビュー詰め合わせ（ログイン、ログアウト、商品詳細、商品購入、カード登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
-  resources :products, only: [:index, :destroy]
+  root 'products#edit'
+  resources :products, only: [:index, :show, :edit, :destroy]
   resources :users, only: [:index, :show]
+  resources :cards, only: [:index]
 end
